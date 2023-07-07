d72c2d30ab0850ef3419e1a86e742f542684fad4
Jongseo Lee <james0614ll@gmail.com>
2023-07-07 12:15:32 +0000

    Lists & conditional contents

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -6,11 +6,27 @@ import "./Expenses.css";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
-
   const selectYearHandler = (selectedYear) => {
-    console.log(selectedYear)
+    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
+  const items = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
+
+  let expensesContent = <p>No contents available</p>;
+
+  if (items.length > 0) {
+    expensesContent = items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,26 +34,7 @@ function Expenses(props) {
           selected={filteredYear}
           onSelectYear={selectYearHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {expensesContent}
       </Card>
     </div>
   );
--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isShowing, setIsShowing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsShowing(false);
+  };
+
+  const addExpenseHandler = () => {
+    setIsShowing(true);
+  };
+  const cancelHandler = () => {
+    setIsShowing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isShowing && (
+        <button onClick={addExpenseHandler}>Add New Expense</button>
+      )}
+      {isShowing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      )}
     </div>
   );
 };
